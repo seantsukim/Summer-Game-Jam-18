d2b47ee2198d6d31ccd66e583cd8ac4b684a10b1
Unknown <kdeppmeier@yahoo.com>
2018-08-18 11:05:28 -0700

    WallofDoom trigger detection

--- SGJ18/Assets/Scripts/WallofDoom.cs
+++ SGJ18/Assets/Scripts/WallofDoom.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class WallofDoom : MonoBehaviour
 {
-    public float velocity;
-
+    public float wallVelocity;
     Rigidbody2D rb;
 
-    Start()
+    void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(wallVelocity, 0);
+    }
+
+    void OnTriggerEnter2D()
+    {
+        //Detect if it collided with Player
+        Debug.Log("Collided");
     }
 }
