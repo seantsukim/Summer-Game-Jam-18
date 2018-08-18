2e8a0673d6c58a44c5dfd1fb69b23f904d21d4f7
Unknown <kdeppmeier@yahoo.com>
2018-08-18 10:28:57 -0700

    WallofDoom WIP

---
+++ SGJ18/Assets/Scripts/WallofDoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallofDoom : MonoBehaviour
+{
+    public float velocity;
+
+    Rigidbody2D rb;
+
+    Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+}
