9ef7155d5f23daeb64b47dde0955beb80e7bc4ae
Unknown <kdeppmeier@yahoo.com>
2018-08-18 10:11:30 -0700

    Main Menu and basic button functionality

---
+++ SGJ18/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void ChangeScene(string newScene)
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
