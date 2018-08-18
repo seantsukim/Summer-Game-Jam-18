445302aa07fc70f5c2123c151ace1de8ed1c4142
Unknown <kdeppmeier@yahoo.com>
2018-08-18 11:49:37 -0700

    Interactable objects, vents

---
+++ SGJ18/Assets/Scripts/Interactable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public KeyCode keyTrigger;
+    //public string className;
+    public string methodName;
+
+    bool inRange = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyTrigger) && inRange)
+        {
+            //Perform task
+            Debug.Log(keyTrigger + " pressed");
+            SendMessage(methodName);
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        //Detect if player
+        inRange = true;
+        Debug.Log("Entering radius of " + gameObject.name);
+    }
+
+    void OnTriggerExit2D()
+    {
+        //Detect if player
+        inRange = false;
+        Debug.Log("Exiting radius" + gameObject.name);
+    }
+}
---
+++ SGJ18/Assets/Scripts/Vent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vent : MonoBehaviour
+{
+    public GameObject partnerVent;
+
+    void MoveToPartnerVent()
+    {
+        //GameObject player = GameObject.FindWithTag("Player");
+        //player.transform.position = partnerVent.transform.position;
+        GameObject.FindWithTag("Player").transform.position = partnerVent.transform.position;
+    }
+}
