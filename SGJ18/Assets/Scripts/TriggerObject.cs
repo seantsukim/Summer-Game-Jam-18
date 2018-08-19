01a8cc69668d56453e4e48878480b4f187820216
Unknown <kdeppmeier@yahoo.com>
2018-08-19 11:58:31 -0700

    Level2 almost complete

---
+++ SGJ18/Assets/Scripts/TriggerObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObject : MonoBehaviour {
+
+    public GameObject triggeredObject;
+
+    //Trigger that sets triggeredObject active when the player enters
+
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        //Debug.Log("triggered!!!");
+        if (target.tag == "Player")
+        {
+            Debug.Log(target.gameObject);
+            triggeredObject.SetActive(true);
+            this.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
