03c7bc984db099a14e81b712455218b0330cb896
Unknown <kdeppmeier@yahoo.com>
2018-08-19 16:42:56 -0700

    Made Music Manager
    Copy MusicManager object from MainMenu to all scenes.  Level3 will need audiosource with own track.

---
+++ SGJ18/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private static GameObject instance = null;
+
+	// Use this for initialization
+	void Awake()
+    {
+        //Debug.Log(instance);
+        if (instance != null)
+        {
+            if (SceneManager.GetActiveScene().name == "Level3")
+            {
+                instance.gameObject.GetComponent<AudioSource>().enabled = false;
+            }
+            else
+            {
+                instance.gameObject.GetComponent<AudioSource>().enabled = true;
+            }
+        }
+
+        if (instance == null)
+        {
+            instance = this.gameObject;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
