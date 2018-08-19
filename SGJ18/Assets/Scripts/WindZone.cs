a2fd085bc3a0fa62d33e98262ca0170ecb4200ff
Will G <wgauthie@usc.edu>
2018-08-19 11:22:11 -0700

    Working on wind zone

---
+++ SGJ18/Assets/Scripts/WindZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindZone : MonoBehaviour
+{
+    public Vector2 windDirection;
+    public float windStrength;
+
+    void OnValidate()
+    {
+        windDirection.Normalize();
+        windStrength = Mathf.Abs(windStrength);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnDrawGizmos()
+    {
+        Ray ray = new Ray(transform.position, windDirection * windStrength);
+        Gizmos.DrawRay(ray);
+    }
+}
