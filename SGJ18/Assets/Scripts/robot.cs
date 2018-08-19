25c905ca89ede96dcd3e4e29cef8e019a5810429
seantsukim <seantsukim@gmail.com>
2018-08-18 17:47:51 -0700

    started robot script

---
+++ SGJ18/Assets/Scripts/robot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot : MonoBehaviour {
+	//using enum to hold the robot's current state
+	public enum robotAction
+	{
+		idle,
+		laser,
+		swing
+	}
+	//keep track of what the robot is currently doing
+	private robotAction currentState;
+
+	// Use this for initialization
+	void Start () {
+		currentState = robotAction.idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(currentState)
+		{
+			case robotAction.idle:
+				Debug.Log(1);
+				break;
+			case robotAction.laser:
+				Debug.Log(2);
+				break;
+			case robotAction.swing:
+				Debug.Log(3);
+				break;
+		}
+	}
+}
